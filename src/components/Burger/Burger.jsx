6f828bfaea874e78aad8bb1d300534d3a786a986
dave <dsersch640@gmail.com>
2018-04-30 13:49:12 -0700

    added state to burgerbuilder and output to burger comp

--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -3,11 +3,17 @@ import classes from './Burger.css'
 import BurgerIngredients from './BurgerIngredients/BurgerIngredients'
 
 const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients) // creates an array of each key (ingredient)
+        .map(igKey => {  // maps over that array for each ingredient
+            return [...Array(props.ingredients[igKey])].map((_, i) => {  // creates an empty array with a length set to the value of the key (ingredient)
+                return <BurgerIngredients key={igKey + i} type={igKey} /> // returns the jsx code into the created array
+            })
+        })
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredients type="bread-top" />
-            <BurgerIngredients type="cheese" />
-            <BurgerIngredients type="meat" />
+            {transformedIngredients}
             <BurgerIngredients type="bread-bottom" />
         </div>
     );

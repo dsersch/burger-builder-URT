664568483b54c88fa3b7232696e73dcb62a04c26
dave <dsersch640@gmail.com>
2018-04-30 14:33:34 -0700

    end of lesson 120, added controls

--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -3,12 +3,18 @@ import classes from './Burger.css'
 import BurgerIngredients from './BurgerIngredients/BurgerIngredients'
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients) // creates an array of each key (ingredient)
+    let transformedIngredients = Object.keys(props.ingredients) // creates an array of each key (ingredient)
         .map(igKey => {  // maps over that array for each ingredient
             return [...Array(props.ingredients[igKey])].map((_, i) => {  // creates an empty array with a length set to the value of the key (ingredient)
                 return <BurgerIngredients key={igKey + i} type={igKey} /> // returns the jsx code into the created array
             })
-        })
+        }).reduce((arr, el) => {
+            return arr.concat(el);
+        }, [])
+
+    if(transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }
 
     return (
         <div className={classes.Burger}>
